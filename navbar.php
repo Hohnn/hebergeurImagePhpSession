a13055676aae5eaf71ae8c89508f5abbb36d9c24
unknown <jean-bateee@hotmail.fr>
2021-06-23 09:21:44 +0200

    jeje

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<div class="row bg-white fixed-bottom p-2 ">
+<div class="row fixed-bottom p-2 navbar">
     <a class="col" href="gallery.php" class="link-primary"><img class="col" src="./assets/imgSite/house.svg"  height="30px" alt="home"></a>
     <a class="col" href="upload.php" class="link-secondary"> <img class="col"src="./assets/imgSite/plus-square.svg"  height="20px" alt="plus"></a>
     <a class="col" href="profil.php" class="link-success"><img class="col"src="./assets/imgSite/person.svg"  height="30px" alt="profil"></a>
